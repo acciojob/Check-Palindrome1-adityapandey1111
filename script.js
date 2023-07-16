dcea7dfb4135903c74720b5dc5d36db9f5ea275b
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-07-16 19:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(s){
+	s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+	return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
+
